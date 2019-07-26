f4188f0f5d049c252abf8f9412cecb9b1140b44a
Tomasz Skrzypiec <tomasz.skrzypiec@orange.com>
2019-07-26 11:42:52 +0200

    first commit

---
+++ BrowserAutomationJavaShop/src/test/java/pl/TS/AutomationTestJavaShop/RunnerTest.java
@@ -0,0 +1,16 @@
+package pl.TS.AutomationTestJavaShop;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.junit.Cucumber;
+
+
+@RunWith(Cucumber.class)
+@Cucumber.Options(
+		features = {"src/test/resource/features/"}
+		,glue={"stepDefinition"}
+		)
+
+public class RunnerTest {	
+	
+}
