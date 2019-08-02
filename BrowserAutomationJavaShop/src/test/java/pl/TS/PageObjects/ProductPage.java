7ccc0e3083b43039b4d5614657640dd7e4c8c9e1
Tomasz Skrzypiec <tomasz.skrzypiec@orange.com>
2019-08-02 13:55:43 +0200

    Bigdecimal case

--- BrowserAutomationJavaShop/src/test/java/pl/TS/PageObjects/ProductPage.java
+++ BrowserAutomationJavaShop/src/test/java/pl/TS/PageObjects/ProductPage.java
@@ -8,15 +8,14 @@ import org.openqa.selenium.WebDriver;
 
 public class ProductPage extends AbstractPage {
 	
-	final String expectedResult = new String("56.00");//zmienilem na format 56.00 zrob dodawanie total ptoducts + total shipmetn
-	final String totalShipping = new String("2"); //dodalem , dodaj locator 
-	//final String totalProducts = new String ()
-	//final String quantity = new String zrob cala logike liczenia product razy ilosc + shipping
-	final static BigDecimal EXPECTEDRESULT = new BigDecimal(0);
+	final String expectedResult = new String("56.00");
+	
+	/*final static BigDecimal EXPECTEDRESULT = new BigDecimal("0");
+	final static BigDecimal TOTALSHIPPING = new BigDecimal("0");
+	final static BigDecimal QUANTITY = new BigDecimal("0");
+	final static BigDecimal TOTALPRODUCTS = new BigDecimal("0");  */
 	
 	
-	
-	//= totalshipping + total products;
 	final String cartTotal = new String("//span[@class='ajax_block_cart_total']");
 	protected By byCartTotal = By.xpath(cartTotal);
 	
