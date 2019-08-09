d9ffb5ed7f1bc98efd24ca962b38b659cb7be52e
Tomasz Skrzypiec <tomasz.skrzypiec@orange.com>
2019-08-09 15:22:11 +0200

    BigDecimal case cd

--- BrowserAutomationJavaShop/src/test/java/pl/TS/PageObjects/ProductPage.java
+++ BrowserAutomationJavaShop/src/test/java/pl/TS/PageObjects/ProductPage.java
@@ -10,10 +10,17 @@ public class ProductPage extends AbstractPage {
 	
 	final String expectedResult = new String("56.00");
 	
-	/*final static BigDecimal EXPECTEDRESULT = new BigDecimal("0");
+/*	final static BigDecimal EXPECTEDRESULT = new BigDecimal("0");
 	final static BigDecimal TOTALSHIPPING = new BigDecimal("0");
 	final static BigDecimal QUANTITY = new BigDecimal("0");
-	final static BigDecimal TOTALPRODUCTS = new BigDecimal("0");  */
+	final static BigDecimal TOTALPRODUCTS = new BigDecimal("0");  
+	final static BigDecimal TOTAL = new BigDecimal("0");  
+	BigDecimal TotalSum = TOTALPRODUCTS.add(TOTALSHIPPING);
+	final String TotalProducts = new String ("//span[@class='ajax_block_products_total']");
+	final String TotalShipping = new String ("//span[@class='ajax_cart_shipping_cost']");
+	final String Total = new String ("//span[@class='ajax_block_cart_total']");  */
+	
+	
 	
 	
 	final String cartTotal = new String("//span[@class='ajax_block_cart_total']");
